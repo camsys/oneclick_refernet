1bebae8421448ea654168d4378e798a6b135fcc4
Nicolas Garcia <ngarcia@camsys.com>
2017-08-22 14:41:38 -0400

    Delete deprecated test folder

--- test/models/oneclick_refernet/service_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-module OneclickRefernet
-  class ServiceTest < ActiveSupport::TestCase
-    # test "the truth" do
-    #   assert true
-    # end
-  end
-end
--- test/oneclick_refernet_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class OneclickRefernet::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, OneclickRefernet
-  end
-end
