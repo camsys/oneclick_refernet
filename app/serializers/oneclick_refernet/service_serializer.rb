49b9136f426426b1208d06804628ae2d29adbf9a
Derek Edwards <dedwards8@gmail.com>
2017-08-25 16:53:21 -0400

    test

--- app/serializers/oneclick_refernet/service_serializer.rb
+++ app/serializers/oneclick_refernet/service_serializer.rb
@@ -7,11 +7,58 @@ module OneclickRefernet
                :lat,
                :lng,
                :address,
-               :test
+               :place
 
-    def test
-      "testing"
+    def place
+    	formatted_address
+    	geometry
     end
+
+    def address_components
+      address_components = []
+
+      #street_number
+      if object.street_number
+        address_components << {long_name: object.street_number, short_name: object.street_number, types: ['street_number']}
+      end
+
+      #Route
+      if object.route
+        address_components << {long_name: object.route, short_name: object.route, types: ['route']}
+      end
+
+      #City
+      if object.city
+        address_components << {long_name: object.city, short_name: object.city, types: ["locality", "political"]}
+      end
+
+      #State
+      if object.state
+        address_components << {long_name: object.zip, short_name: object.zip, types: ["postal_code"]}
+      end
+
+      #Zip
+      if object.zip
+        address_components << {long_name: object.state, short_name: object.state, types: ["administrative_area_level_1","political"]}
+      end
+
+      return address_components
+
+    end
+
+    def formatted_address
+        object.address
+    end
+
+  def geometry
+    {
+      location: {
+        lat: object.lat.to_f,
+        lng: object.lng.to_f,
+      }
+    }
+  end
+
     
   end
 end
