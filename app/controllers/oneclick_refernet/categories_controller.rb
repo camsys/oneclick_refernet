63da8d920de6b4135fd6ebfefebb450aefd79be2
Derek Edwards <dedwards8@gmail.com>
2017-10-10 19:39:26 -0400

    Update service translations

--- app/controllers/oneclick_refernet/categories_controller.rb
+++ app/controllers/oneclick_refernet/categories_controller.rb
@@ -2,6 +2,8 @@ module OneclickRefernet
   class CategoriesController < ApplicationController
     
     def index
+      puts 'test'
+      puts @traveler.ai 
       render json: Category.confirmed
     end
     
