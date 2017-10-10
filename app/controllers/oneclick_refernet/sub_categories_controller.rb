b44d1cdd2b2abc822bb256c6ddbc5d8489dc1a02
Derek Edwards <dedwards8@gmail.com>
2017-10-10 19:37:54 -0400

    Update service translations

--- app/controllers/oneclick_refernet/sub_categories_controller.rb
+++ app/controllers/oneclick_refernet/sub_categories_controller.rb
@@ -2,6 +2,8 @@ module OneclickRefernet
   class SubCategoriesController < ApplicationController
     
     def index
+      puts 'test'
+      puts @traveler.ai 
       @category = Category.find_by(name: params[:category])
       
       render json: (@category.try(:sub_categories).try(:confirmed) || [])
