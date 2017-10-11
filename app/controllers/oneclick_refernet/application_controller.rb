57d681154e47040e9947468204f2d4b8d7a80e93
Derek Edwards <dedwards8@gmail.com>
2017-10-11 11:32:10 -0400

    Test

--- app/controllers/oneclick_refernet/application_controller.rb
+++ app/controllers/oneclick_refernet/application_controller.rb
@@ -1,12 +1,5 @@
 module OneclickRefernet
   class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
-    before_action :get_locale 
-
-    def get_locale
-    	puts 'doing this'
-    	puts params.ai 
-    	@locale = params[:locale] || :en
-    end
   end
 end
--- app/controllers/oneclick_refernet/categories_controller.rb
+++ app/controllers/oneclick_refernet/categories_controller.rb
@@ -2,6 +2,7 @@ module OneclickRefernet
   class CategoriesController < ApplicationController
     
     def index
+      @locale = params[:locale] || :en
       render json: Category.confirmed, scope: {locale: @locale}
     end
     
