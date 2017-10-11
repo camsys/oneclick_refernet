de64ac3f422fc60dd5217f30cd0d0f99ea6666c4
Derek Edwards <dedwards8@gmail.com>
2017-10-11 11:26:50 -0400

    Test

--- app/serializers/oneclick_refernet/categorical_serializer.rb
+++ app/serializers/oneclick_refernet/categorical_serializer.rb
@@ -5,6 +5,9 @@ module OneclickRefernet
     attributes :name, :service_count
 
     def name
+    	puts 'derek derek'
+    	puts scope.ai 
+    	
       object.translated_name(scope[:locale])
     end
     
