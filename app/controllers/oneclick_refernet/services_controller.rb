2551c7b576b833c5ae63b5c98cc3cf30dd06f3a4
Derek Edwards <dedwards8@gmail.com>
2017-10-11 11:33:33 -0400

    Test

--- app/controllers/oneclick_refernet/services_controller.rb
+++ app/controllers/oneclick_refernet/services_controller.rb
@@ -2,8 +2,8 @@ module OneclickRefernet
   class ServicesController < ApplicationController
     
     def index
+      @locale = params[:locale] || :en
       @sub_sub_category = SubSubCategory.find_by(name: params[:sub_sub_category])
-      @derek = "derek"
       render json: (@sub_sub_category.try(:services).try(:confirmed) || []), scope: {locale: @locale}
     end
     
--- app/controllers/oneclick_refernet/sub_categories_controller.rb
+++ app/controllers/oneclick_refernet/sub_categories_controller.rb
@@ -2,6 +2,7 @@ module OneclickRefernet
   class SubCategoriesController < ApplicationController
     
     def index
+      @locale = params[:locale] || :en
       @category = Category.find_by(name: params[:category])
       render json: (@category.try(:sub_categories).try(:confirmed) || []), scope: {locale: @locale}
     end
