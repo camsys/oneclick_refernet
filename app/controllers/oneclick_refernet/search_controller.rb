005783e8668361034eaef0a90aa365650083e16b
Nicolas Garcia <ngarcia@camsys.com>
2017-10-19 09:46:18 -0400

    Serve back category/service models with search results

--- app/controllers/oneclick_refernet/search_controller.rb
+++ app/controllers/oneclick_refernet/search_controller.rb
@@ -3,9 +3,14 @@ module OneclickRefernet
     
     # GET /search
     def search
+      @locale = params[:locale] || :en
       @results = OneclickRefernet::KeywordSearcher.new.search(params[:term])
+      
       serialized_results = @results.map do |result|
-        OneclickRefernet::SearchResultSerializer.new(result).serializable_hash
+        OneclickRefernet::SearchResultSerializer.new(
+          result, 
+          scope: { locale: @locale}
+        ).serializable_hash
       end
                             
       render json: { results: serialized_results }
