b933c819bfcca2c2dff4ae0f66aef021a8618d48
Derek Edwards <dedwards8@gmail.com>
2017-11-01 12:05:32 -0400

    Meters is controllable

--- app/controllers/oneclick_refernet/sub_sub_categories_controller.rb
+++ app/controllers/oneclick_refernet/sub_sub_categories_controller.rb
@@ -5,7 +5,7 @@ module OneclickRefernet
       @locale = params[:locale] || :en
       @sub_category = SubCategory.find_by(name: params[:sub_category])
       render json: (@sub_category.try(:sub_sub_categories).try(:confirmed) || []), 
-             scope: {locale: @locale, lat: params[:lat], lng: params[:lng]}
+             scope: {locale: @locale, lat: params[:lat], lng: params[:lng], meters: (params[:meters] || 48280.3).to_f}
     end
     
   end
