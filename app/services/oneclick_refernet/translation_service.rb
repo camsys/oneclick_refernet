2ce3ab39b87a7f069ae7a81e39f947ecb088ef0a
Nicolas Garcia <ngarcia@camsys.com>
2017-11-06 13:29:38 -0500

    Properly namespace references to translation table

--- app/services/oneclick_refernet/translation_service.rb
+++ app/services/oneclick_refernet/translation_service.rb
@@ -3,13 +3,13 @@ module OneclickRefernet
 	class TranslationService
 
 		def set key, locale, value
-			translation = Translation.where(key: key, locale: locale.to_s).first_or_initialize
+			translation = OneclickRefernet::Translation.where(key: key, locale: locale.to_s).first_or_initialize
 			translation.value = value
 			translation.save 
 		end	
 
 		def get key, locale=:en
-			Translation.find_by(key: key, locale: locale.to_s).try(:value)
+			OneclickRefernet::Translation.find_by(key: key, locale: locale.to_s).try(:value)
 		end	
 	end
 end
