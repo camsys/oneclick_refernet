bad6b515bfcb3e40327ec78632861c60e74fe53b
Nicolas Garcia <ngarcia@camsys.com>
2017-11-14 13:30:45 -0500

    Updaet services controller to use code

--- app/controllers/oneclick_refernet/services_controller.rb
+++ app/controllers/oneclick_refernet/services_controller.rb
@@ -3,7 +3,7 @@ module OneclickRefernet
     
     def index
       @locale = params[:locale] || I18n.default_locale
-      @sub_sub_category = SubSubCategory.find_by(name: params[:sub_sub_category])
+      @sub_sub_category = SubSubCategory.find_by(code: params[:sub_sub_category])
       render json: (@sub_sub_category.try(:services).try(:confirmed) || []), 
              scope: {locale: @locale}
     end
