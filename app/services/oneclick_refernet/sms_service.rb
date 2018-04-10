b1542c69673299b89bfb1d1cef958482e7f2a48c
Derek Edwards <dedwards8@gmail.com>
2018-04-10 15:28:00 -0400

    temp

--- app/services/oneclick_refernet/sms_service.rb
+++ app/services/oneclick_refernet/sms_service.rb
@@ -13,7 +13,7 @@ module OneclickRefernet
       sns = Aws::SNS::Client.new(
         region: ENV['AWS_ACCESS_KEY_ID'],
         access_key_id: ENV['AWS_ACCESS_KEY_ID'] , 
-        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+        secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'])
      
       sns.publish({phone_number: '+14233092124', message: 'test message'})
 
